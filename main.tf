cfbf5f9a7a7238c6ab57e306b14721b663efa1ee
Kapil Arora <512729+kaparora@users.noreply.github.com>
2021-01-20 13:17:54 +0100

    using westeurope

--- main.tf
+++ main.tf
@@ -2,6 +2,6 @@
 module "machine-learning" {
   source  = "app.terraform.io/JoeStack/machine-learning/azure"
   version = "0.0.4"
-  location = "northeurope"
+  location = "westeurope"
   resource_group = "kapil-aml"
 }
