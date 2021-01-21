795513bfb21041ac7fa4f679f0f4960f93647169
Kapil Arora <kapil@kapil-mac.localdomain>
2021-01-21 11:43:44 +0100

    initial checking azure ml ws

--- main.tf
+++ main.tf
@@ -1,1 +1,6 @@
-
+module "machine-learning" {
+  source  = "app.terraform.io/JoeStack/machine-learning/azure"
+  version = "0.0.5"
+  resource_group = var.resource_group
+  location = var.location
+}
