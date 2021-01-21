ae703713ceb1f722f7fe3218e15d4fff300a6578
Kapil Arora <512729+kaparora@users.noreply.github.com>
2021-01-21 10:01:54 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 
 module "machine-learning" {
-  source  = "app.terraform.io/kapil-org/machine-learning/azure"
-  version = "0.0.3"
+  source  = "app.terraform.io/JoeStack/machine-learning/azure"
+  version = "0.0.5"
   resource_group = var.resource_group
   location = var.location
 }
