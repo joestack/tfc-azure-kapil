faa863a25011f710f67d0a9f6ea3165d4b83e83c
Kapil Arora <512729+kaparora@users.noreply.github.com>
2021-01-21 09:02:26 +0100

    added outputs

---
+++ output.tf
@@ -0,0 +1,8 @@
+output "jumphost_ip" {
+  value = module.machine-learning.jumphost_ip
+}
+  
+  
+output "machine_learning_workspace_name" {
+  value = module.machine-learning.machine_learning_workspace_name
+}
