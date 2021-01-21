4a4ba08338e75daa056cb70b97e8aff1c8ce0431
Kapil Arora <512729+kaparora@users.noreply.github.com>
2021-01-21 10:01:03 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,16 @@
 
+module "machine-learning" {
+  source  = "app.terraform.io/kapil-org/machine-learning/azure"
+  version = "0.0.3"
+  resource_group = var.resource_group
+  location = var.location
+}
+  
+output "jumphost_ip" {
+  value = module.machine-learning.jumphost_ip
+}
+  
+  
+output "machine_learning_workspace_name" {
+  value = module.machine-learning.machine_learning_workspace_name
+}
